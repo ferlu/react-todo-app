4886c0b09e15257f16016e2dfce8f74ecb30ab2e
Fernanda Luna <ferluna.rm@gmail.com>
2021-08-04 18:18:42 -0500

    * Handle change in todo items

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,12 @@
-function handleCheck() {
-  console.log("Changed!");
-}
-
 function TodoItem(props) {
   return (
     <div className="todo-item">
       <input
         type="checkbox"
-        checked={props.data.completed}
-        onChange={handleCheck}
+        checked={props.item.completed}
+        onChange={() => props.handleChange(props.item.id)}
       />
-      <label htmlFor="task1">{props.data.text}</label>
+      <label htmlFor="task1">{props.item.text}</label>
     </div>
   );
 }
