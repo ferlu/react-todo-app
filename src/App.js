c9162014fdc2472911f94bbe0436ec5f23b6ec59
Fernanda Luna <ferluna.rm@gmail.com>
2021-08-04 13:03:28 -0500

    * Handle events * update dependencies * Add github pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import React from "react";
 import "./App.css";
 import TodoItem from "./components/TodoItem";
 import todosData from "./todosData";
 
-function App() {
-  const todoItems = todosData.map((item) => (
-    <TodoItem key={item.id} data={item} />
-  ));
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData,
+    };
+  }
+  render() {
+    const todoItems = this.state.todos.map((item) => (
+      <TodoItem key={item.id} data={item} />
+    ));
 
-  return <div className="todo-list">{todoItems}</div>;
+    return <div className="todo-list">{todoItems}</div>;
+  }
 }
 
 export default App;
